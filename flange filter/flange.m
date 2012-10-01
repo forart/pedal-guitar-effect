595d27f6713699cbfc71c9a6a3462eb789788e00
framework13@gmail.com <framework13@gmail.com@232767ae-00e1-59b8-5f43-81c466a1c032>
2012-10-01 20:57:09 +0000

    Code modified: Plot implemented, input and output introduced, some mistakes corrected

--- flange filter/flange.m
+++ flange filter/flange.m
@@ -1,4 +1,10 @@
-function [y]=flange(fs, v, y, r)
+clear all;
+close all;
+
+infile = 'song.wav';
+% read in wav sample and obtain some values
+[ x, fs, N ] = wavread(infile);
+
 %[y] = flange(fs, v, x, r)
 %
 %   This is a basic flanging effect.
@@ -11,19 +17,40 @@ function [y]=flange(fs, v, y, r)
 %   Example:     
 %      >>y = flange(fs,0.002,x,0.5);%
 %
-%   See also WAVREAD and SOUND
-%
-%Version 1.0
-%Once we have the input and output variables, adapt it ^^
 
-md= ceil(v*fs); %we start to create the signal variating the sample rate
+%%%%%%% EFFECT COEFFICIENTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Variation factor
+v = 0.002;
+% Rate
+r=0.5;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [y]=flange(fs, v, y, r);
+
+%we start to create the signal variating the sample rate
+md= ceil(v*fs);
 n=1:length(y)+md;
 v=round(v*fs);
 z=zeros(md,1);
-m=max(abs(y));
 y=[z;y;z];
 rr=2*pi/round(fs*r);
 b=round((v/2)*(1-cos(rr.*n)));
 y=y(n+md)+y(n+md-b); %we combine the two signals to create the flange effect
+
+%now we normalise
+m=max(abs(y));
 m=m/max(abs(y));
-y=m*y; %the final signal = the combination of both and shortened to the initial lenght
+
+%the final signal = the combination of both and shortened to the initial lenght
+y=m*y;
+
+% write output wav files
+wavwrite(y, fs, N, 'flanger.wav');
+
+%and we show it
+figure(1)
+hold on
+plot(x,'r');
+plot(yb,'b');
+title('Flanger and Original Signal');
