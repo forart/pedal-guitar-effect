1a2d22379453ed00bdd3b2a65a543520518cffa6
framework13@gmail.com <framework13@gmail.com@232767ae-00e1-59b8-5f43-81c466a1c032>
2012-10-01 08:05:49 +0000

    Changes in the code: Comments

--- flange filter/flange.m
+++ flange filter/flange.m
@@ -5,21 +5,18 @@ function [y]=flange(fs, v, y, r)
 %
 %      fs = Sample rate
 %      v = Variation.
-%      x = Input audio signal. This should be a column 
-%          vector.
+%      x = Input audio signal. This should be a column vector.
 %      r = Rate.
 %
-%   Example:
-%     
-%      >>y = flange(fs,0.002,x,0.5);
-%
+%   Example:     
+%      >>y = flange(fs,0.002,x,0.5);%
 %
 %   See also WAVREAD and SOUND
 %
 %Version 1.0
-%Coded by: Stephen G. McGovern, date: 08.03.03
+%Once we have the input and output variables, adapt it ^^
 
-md= ceil(v*fs);
+md= ceil(v*fs); %we start to create the signal variating the sample rate
 n=1:length(y)+md;
 v=round(v*fs);
 z=zeros(md,1);
@@ -27,6 +24,6 @@ m=max(abs(y));
 y=[z;y;z];
 rr=2*pi/round(fs*r);
 b=round((v/2)*(1-cos(rr.*n)));
-y=y(n+md)+y(n+md-b);
+y=y(n+md)+y(n+md-b); %we combine the two signals to create the flange effect
 m=m/max(abs(y));
-y=m*y;
+y=m*y; %the final signal = the combination of both and shortened to the initial lenght
